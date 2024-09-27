565466184eeb6025742e8ffc4cf7c8664a663eac
BielRiera-ITB2425 <biel.riera.7e8@itb.cat>
2024-09-27 08:23:39 +0200

    Projectes TA02

---
+++ TA02/ComprovarEdat.py
@@ -0,0 +1,23 @@
+"""
+Biel Riera Toro
+
+26/09/2024
+
+ASIXd M03 UF1 A1
+
+Descripció: Exemple dels apunts
+
+"""
+
+# Programa que demana l'edat i diu si ets major d'edat.
+
+edat=int(input("Quina edat tens?"))
+
+if edat>=18:
+
+    print("Ets major d'edat")
+
+elif edat<=18:
+    print("Ets menor d'edat")
+
+print("Programa Finalitzat")
